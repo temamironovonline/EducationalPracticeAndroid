0c04f2e85444c506469a708bada6f2715c13210c
temamironovonline <a.sergeichev03@mail.ru>
2023-03-20 18:37:16 +0300

    complete2

--- app/src/main/java/com/example/educationalpracticeandroid/MusicActivity.java
+++ app/src/main/java/com/example/educationalpracticeandroid/MusicActivity.java
@@ -1,6 +1,18 @@
 package com.example.educationalpracticeandroid;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+    }
 }
